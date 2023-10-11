ed298754ca1295eb200574cfdbdc3e686aefa421
MaximeSchuybroeck <maxime.schuybroeck@student.kuleuven.be>
2023-10-11 15:58:14 +0200

    rename dir

---
+++ ex2/ex2.c
@@ -0,0 +1,6 @@
+#include "ex2Func.c"
+int main(){
+	//call a function in another file
+	printAllSizes();
+	return 0;
+}
---
+++ ex2/ex2Func.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "ex2.h"
+void printAllSizes(){
+	printf("size of int = %ld\n",sizeof(int));
+	printf("size of float = %ld\n",sizeof(float));
+	printf("size of  char = %ld\n",sizeof(char));
+	printf("size of  void = %ld\n",sizeof(void));
+	printf("size of  double = %ld\n",sizeof(double));
+	return;
+}
